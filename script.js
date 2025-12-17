548487069b7382fa041f39200f8c6c1f5492804f
UNETI SCC <minhviptnguyen0101@gmail.com>
2025-12-17 08:57:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 document.addEventListener('contextmenu', event => event.preventDefault());
 
-fetch("../nav.html")
+const isRoot = location.pathname.endsWith('/') || location.pathname.endsWith('index.html');
+const navPath = isRoot ? 'nav.html' : '../nav.html';
+
+fetch(navPath)
   .then(res => res.text())
   .then(data => {
     document.getElementById("nav-placeholder").innerHTML = data;
