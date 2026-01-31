60c13b2f0b351ab3b034d8d0525ffd3d933a5ad6
UNETI SCC <minhviptnguyen0101@gmail.com>
2026-01-31 10:04:18 +0700

    Add files via upload

---
+++ script3Nam.js
@@ -0,0 +1,15 @@
+const items = document.querySelectorAll('.timeline-item');
+
+function showTimeline() {
+  const trigger = window.innerHeight * 0.85;
+
+  items.forEach(item => {
+    const top = item.getBoundingClientRect().top;
+    if (top < trigger) {
+      item.classList.add('show');
+    }
+  });
+}
+
+window.addEventListener('scroll', showTimeline);
+showTimeline();
